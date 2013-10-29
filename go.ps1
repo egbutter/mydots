6adc5a363d884587ed495f1f3170ec4e60acdf43
egbutter <hola@ericbeurre.com>
2013-10-29 18:29:16 -0400

    updated this script to reflect my new layout

--- go.ps1
+++ go.ps1
@@ -3,7 +3,40 @@
 ## this is still windows, so needs dos endings (no d2u!)
 
 set-executionpolicy remotesigned
-set-variable -name HOME -value $env:USERPROFILE -force
+$myhome = $env:userprofile  # bc forcing $home to something else is a pain
+
+
+# explicitly set currentuserallhosts to %userprofile%
+$psprof = $profile.currentuserallhosts
+if (test-path $psprof) { move $psprof "$($psprof).bak" }
+cmd /c mklink /H $psprof "$($myhome)\.psprofile.ps1"
+echo "pointed powershell currentuserallhosts to userprofile"
+
+
+# make sure the env vars are set correctly, inconsistent on win32 
+$psmods = join-path $myhome ".psmodules"
+if ($env:psmodulepath.split(";") -notcontains $psmods) {
+    $env:psmodulepath += $psmods
+    [Environment]::SetEnvironmentVariable("psmodulepath", $psmods, "User")
+    echo "pointed powershell psmodules to userprofile"
+}
+
+$vimpath = join-path $myhome ".vim"
+if ($env:vimruntime.split(";") -notcontains $vimpath) {
+    $env:vimruntime += $vimpath
+    [Environment]::SetEnvironmentVariable("vimruntime", $vimpath, "User")
+    echo "pointed vimruntime to userprofile"
+}
+
+$xlpath = join-path $myhome ".xlstart"
+$xlappdata "$env:appdata\Microsoft\Excel\XLSTART"
+if (test-path $xlappdata) 
+{
+    move $xlappdata "$($xlappdata).bak"
+    junction -s $xlpath $xlappdata
+    echo "pointed XLSTART to userprofile"
+}
+
 
 (new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex
 import-module psget
@@ -15,33 +48,24 @@ install-module poshcode
 install-module find-string
 install-module psurl
 
-# get-poshcode cmatrix -Destination "$($home)/.psmods"
-# error proxycred https://getsatisfaction.com/poshcode/topics/error_with_get_poshcode
+#BROKEN: get-poshcode cmatrix -Destination $psmods
+# "error proxycred" https://getsatisfaction.com/poshcode/topics/error_with_get_poshcode
 
-$psprof = $profile.currentuserallhosts
-if (test-path $psprof) { move $psprof "$($psprof).bak" }
-cmd /c mklink /H $psprof "$($pwd)\_psprofile.ps1"
-
-#$xlpath = "$env:appdata\Microsoft\Excel\XLSTART"
-
-
-if (!(test-path "$($home)\vimfiles")) { mkdir "$($home)\vimfiles" }
 
 function LinkFile ([string]$tolink) {
 
-    $source="$($pwd)\$(tolink)"
+    $source="$(join-path $pwd $tolink)"
 
-    If ($tolink -eq "_vim") { 
-        $target="$($home)\vimfiles"  # peculiar win32 gvim inconsistency
-    } Else {
-        $target="$($home)\$($tolink)"
-    }
+    $dotlink = $tolink -replace "^_", "."
+    $target="$(join-path $myhome $dotlink)"
 
     if (test-path $target) { move $target "$($target).bak" }
 
     if ($target.psiscontainer) {
+        echo "junction -s $target $source"
         junction -s $target $source
     } else {
+        echo "cmd /c mklink /H $target $source"
         cmd /c mklink /H $target $source
     }
 }
@@ -71,9 +95,10 @@ try {
     import-module virtualenvwrapper
 } catch {
     try {
+        echo "trying to install virtualenvwrapper ..."
         pip install virtualenvwrapper-powershell
     } catch {
-        Throw "ERROR: please pip install virtualenvwrapper-powershell"
+        echo "WARNING: python and/or pip and/or virtualenvwrapper-powershell not installed"
     }
 }
 
@@ -81,11 +106,11 @@ try {
 try {
     pip install pyflakes
 } catch {
-    Throw "ERROR: please pip install pyflakes"
+    echo "WARNING: python and/or pip and/or pyflakes not installed, some vim plugins will not work"
 }
 
 # update all the submodules
-cd "$($home)\vimfiles"
+cd "$($myhome)\vimfiles"
 git submodule sync
 git submodule init
 git submodule update
@@ -93,4 +118,5 @@ git submodule foreach git pull origin master
 git submodule foreach git submodule init
 git submodule foreach git submodule update
 
-set-location $home -PassThru
+set-location $myhome -PassThru
+set-variable -name HOME -value $env:USERPROFILE -force  # may not persist sessions
